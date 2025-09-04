fb47800f5c99c359b636fda311d6e5fdc2f2b78d
shihuaxie <sylvia501628@gmail.com>
2025-09-05 01:49:29 +1000

    Add task5 to concat two strs

---
+++ python-hello_world/6-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+str1 = "Holberton"
+str2 = "School"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+# Resign str1 new value
+str1 = "Welcome to " + str1 + " " + str2
+print(str1 + "!")
