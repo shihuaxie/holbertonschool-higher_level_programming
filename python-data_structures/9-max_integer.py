aa5b515d3f8e89a11b8ee1df5aeb0bc4293f3f96
shihuaxie <sylvia501628@gmail.com>
2025-09-11 00:43:17 +1000

    Fix: fix pycodestyle error

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,8 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
 
-    max_val = my_list[0] #  if the first int is max
+    # if the first int is max
+    max_val = my_list[0]
     for num in my_list:
         if num > max_val:
             max_val = num
