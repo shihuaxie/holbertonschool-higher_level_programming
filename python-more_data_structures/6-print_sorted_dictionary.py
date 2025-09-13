9af124d9369d162c0b109ea4dd504c875ed79f4b
shihuaxie <sylvia501628@gmail.com>
2025-09-13 22:27:04 +1000

    Fix: fix task 6 and task 7

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     # sorted function
     for key in sorted(a_dictionary):
-        print(f"{key} : {a_dictionary[key]}")
+        print(f"{key}: {a_dictionary[key]}")
