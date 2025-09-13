a978f71fbe4c44e9d67f5d4becf7fbe604b521e5
shihuaxie <sylvia501628@gmail.com>
2025-09-13 22:45:50 +1000

    Fix: fix error

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    # covers None and {}
+    if not a_dictionary:
         return None
     else:
         return max(a_dictionary, key=a_dictionary.get)
