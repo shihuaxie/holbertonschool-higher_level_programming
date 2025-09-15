dbf4a539e7eb3b01360389be17d37bd3ce5d07c9
shihuaxie <sylvia501628@gmail.com>
2025-09-15 15:01:47 +1000

    Add task 1 to create a pricate attribute in class Square

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module for class Square"""
+
+
+class Square:
+    """
+        class Suqare
+
+        args: int
+
+        returns: the size of Square
+    """
+
+    __size = 0
+
+    def __init__(self, size):
+        self.__size = size
