8b4da757bebb757ba3be5337afa6723e5a636dac
shihuaxie <sylvia501628@gmail.com>
2025-09-16 14:44:37 +1000

    Add task2 to write a class Square that defines a square by: (based on 1-square.py)

---
+++ python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module for class Square"""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size = 0):
+        """
+        Initialize a new Square.
+
+        Args:
+            size (int): The size of the square (default is 0).
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+
+        if (not isinstance(size, (int))):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
