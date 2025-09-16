c1a47b9a6c254cfad5dceb498f41fb3c8593fa5c
shihuaxie <sylvia501628@gmail.com>
2025-09-16 20:45:00 +1000

    Add task 3 that creats a Public instance method

---
+++ python-classes/3-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Module for class Square"""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """
+        Initialize a new Square.
+
+        Args:
+            size (int): The size of the square (default is 0).
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+
+        if (not isinstance(size, (int))):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    # area is an instance method
+    def area(self):
+        """
+        Calculate the area of the square.
+            
+        Returns:
+            int: the area of the square.
+        """
+        return self.__size ** 2
