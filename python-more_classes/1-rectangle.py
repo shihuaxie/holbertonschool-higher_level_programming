8dbd5e434e27170027e5ed9402ed09ad98f0d639
shihuaxie <sylvia501628@gmail.com>
2025-09-17 22:31:50 +1000

    Fix: remove empty spaces

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -12,7 +12,7 @@ class Rectangle:
             Args:
                 width (int): The width of the rectangle.
                 height (int): The height of the rectangle.
-        """       
+        """
         self.width = width
         self.height = height
 
