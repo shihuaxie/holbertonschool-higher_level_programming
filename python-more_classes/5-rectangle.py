0aee7de08337682e36051ef90bd11c213b5c3394
shihuaxie <sylvia501628@gmail.com>
2025-09-17 23:12:59 +1000

    Add task 5 to create a deletemethod

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+""" Defines an empty Rectangle class. """
+
+
+class Rectangle:
+    """Represents a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """
+            Class Rectangle.
+
+            Args:
+                width (int): The width of the rectangle.
+                height (int): The height of the rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    # ----- width -----
+    @property
+    def width(self):
+        """ Get the width of Rectangle. """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Set the width of Rectangle. """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if (value < 0):
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    # ----- height -----
+    @property
+    def height(self):
+        """ Get the height of Rectangle. """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Set the height of Rectangle. """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if (value < 0):
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    # ----- Public methods -----
+    def area(self):
+        """Return the area of the Rectangle."""
+        # self.height is better than self.__height
+        # this will get from getter - property
+        return (self.height * self.width)
+        print
+
+    def perimeter(self):
+        """Return the perimeter of the Rectangle."""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    # ----- string forms -----
+    def __str__(self):
+        """
+        Return the rectangle drawn with '#' characters.
+        Empty string if width or height is 0.
+        """
+        if self.width == 0 or self.height == 0:
+            return ("")
+        rect_str = ""
+        # height = row, width = how many # in each row
+        for h in range(self.height):
+            rect_str = rect_str + "#" * self.width
+            if h != self.height - 1:  # start new line if it's not last
+                rect_str = rect_str + "\n"
+        return rect_str
+
+    def __repr__(self):
+        """ Return a string that can recreate the object. """
+        # Rectangle(width, height)
+        return f"Rectangle({self.width}, {self.height})"
+
+    # ----- delete method -----
+    def __del__(self):
+        """Called when an instance is deleted."""
+        print("Bye rectangle...")
