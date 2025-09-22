842542255c63342ff6f56dfc4ae826cf386d2ef5
shihuaxie <sylvia501628@gmail.com>
2025-09-22 22:26:02 +1000

    Fix: add two empty lines

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,6 +3,8 @@
 This module defines a function that returns
 the list of available attributes and methods of an object.
 """
+
+
 def lookup(obj):
     """
     Return list of attributes and methods of obj
