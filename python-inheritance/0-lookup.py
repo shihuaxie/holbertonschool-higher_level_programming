2ce6af2035b629bcbf02fce0ddc2efda8332a899
shihuaxie <sylvia501628@gmail.com>
2025-09-22 22:17:18 +1000

    Fix: add docstring

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def lookup(obj):
+    """Return list of attributes and methods of obj"""
     # built-in function dir(obj)
     # returns all methods,attribute, properties of obj
     return dir(obj)
