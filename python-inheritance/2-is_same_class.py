a16fc9f0534c9a691665ccee999302b968255bee
shihuaxie <sylvia501628@gmail.com>
2025-09-22 23:04:11 +1000

    Add task 2 to validate is a obj is exactly an instance os a cls

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,6 +3,7 @@
 A function that validate if the onject is an instance of the specified class
 """
 
+
 def is_same_class(obj, a_class):
     """Return True if obj is exactly an instance of a_class, otherwise False."""
     # type(obj) is a_class is a boolean expression
