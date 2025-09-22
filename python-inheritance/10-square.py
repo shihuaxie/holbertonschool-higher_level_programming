8bc2add7eb0ae3aab2a055bdfbbde88a054cc67e
shihuaxie <sylvia501628@gmail.com>
2025-09-23 00:58:33 +1000

    Add tsk 10 to create cls Square that inherits from Rectangle

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module defines the Square class that inherits from Rectangle.
+"""
+# import BaseGeometry from the previous task module
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class with validated private size."""
+
+    def __init__(self, size):
+        """
+        Initialize a Square.
+
+        Args:
+            size (int): the size of the square (> 0)
+        """
+        # size must be a positive int
+        Rectangle.integer_validator(self, "size", size)
+        # store private size
+        self.__size = size
+        # use super() to use __int__ from parent class Rectangle
+        super().__init__(size, size)
