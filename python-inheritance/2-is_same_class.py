cb1dbe9e50b8324d7788b1fc636dee1cbadb0e2d
shihuaxie <sylvia501628@gmail.com>
2025-09-22 23:05:05 +1000

    Fix: shorten commands

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,6 +5,8 @@ A function that validate if the onject is an instance of the specified class
 
 
 def is_same_class(obj, a_class):
-    """Return True if obj is exactly an instance of a_class, otherwise False."""
+    """
+    Return True if obj is exactly an instance of a_class, otherwise False.
+    """
     # type(obj) is a_class is a boolean expression
     return type(obj) is a_class
