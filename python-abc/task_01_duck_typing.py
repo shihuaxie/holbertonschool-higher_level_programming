c21cea24a9a247e81c5014a32236c0ce5cad2544
shihuaxie <sylvia501628@gmail.com>
2025-09-27 00:22:35 +1000

    Add task 1 to create a duck typing class with multiple methods

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""This is an abstract class Shape with two abcmethods"""
+
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    """
+    A class Shape
+
+    Args:
+        ABC
+    Returns:
+        Nothing
+    """
+
+    @abstractmethod
+    def area(self):
+        """Return numeric area."""
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        """Return numeric perimeter."""
+        pass
+
+
+# Concrete class Circle inherits from Shape
+class Circle(Shape):
+    def __init__(self, radius):
+        if radius <= 0:
+            raise ValueError("radius must be > 0")
+        self._radius = radius
+
+    def area(self):
+        return math.pi * self._radius * self._radius
+
+    def perimeter(self):
+        return 2 * math.pi * self._radius
+
+
+# Concrete class Rectangle inherits from Shape
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        if width <= 0 or height <= 0:
+            raise ValueError("width and height must be > 0")
+        self._width = width
+        self._height = height
+
+    def area(self):
+        area = self._width * self._height
+        return area
+
+    def perimeter(self):
+        perimeter = 2 * (self._width + self._height)
+        return perimeter
+
+
+def shape_info(obj):
+    """Duck-typed function: prints area and perimeter"""
+    print("Area:", obj.area())
+    print("Perimeter:", obj.perimeter())
