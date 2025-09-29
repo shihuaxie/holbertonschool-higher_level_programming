e29fa06d66e16f253c411f88bb16c36d8286b781
shihuaxie <sylvia501628@gmail.com>
2025-09-29 23:13:15 +1000

    Fix: add one more line

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Return a JSON-serializable dict description of a class instance."""
 
+
 def class_to_json(obj):
     """Return the dictionary description of a class instance.
 
