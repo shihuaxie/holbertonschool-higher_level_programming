3d7a4085d9b7654f373dbd7efc9215f8c28822d5
shihuaxie <sylvia501628@gmail.com>
2025-09-29 22:04:59 +1000

    Add task 3 to return the JSON representation of an object

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import json
+
+"""Nameless module"""
+
+
+def to_json_string(my_obj):
+    """Return the JSON representation of an object as a string."""
+    return json.dumps(my_obj)
