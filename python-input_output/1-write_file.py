57a564b8690a4d8901801b852b41d0a4297dca31
shihuaxie <sylvia501628@gmail.com>
2025-09-29 21:42:00 +1000

    Add task 1 to write a func that writes a str to a txt file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module: 1-write_file
+Function: write_file
+Writes a string to a text file (UTF8) and returns the number of characters written.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8).
+    Overwrites the file if it already exists.
+    
+    Args:
+        filename (str): The file name
+        text (str): The string to write
+    
+    Returns:
+        int: Number of characters written
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
