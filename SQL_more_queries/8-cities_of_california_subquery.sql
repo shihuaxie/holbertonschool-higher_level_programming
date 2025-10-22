2a15445ee7c3a59b3670a551a2c8775e86dab6aa
shihuaxie <sylvia501628@gmail.com>
2025-10-22 11:44:54 +0000

    Add task 8 that lists all the cities of California that can be found in the database hbtn_0d_usa

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+--  lists all the cities of California 
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id
+	AND states.name = 'California'
+ORDER BY cities.id ASC;
+
