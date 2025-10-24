09e826070a5cdbe6e90a2d20de7df96e884b2876
shihuaxie <sylvia501628@gmail.com>
2025-10-24 13:10:56 +0000

    Add databse

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- lists all  tv shows with at least one genre
-SELECT tv_shows.title, tv_show_genres.genre.id
-FROM tv_shows, tv_show_genres
-WHERE tv_shows.id = tv_show_genres.show_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
 
