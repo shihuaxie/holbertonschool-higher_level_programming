33f764835856592a8234d2552f3f46c6ee90006c
shihuaxie <sylvia501628@gmail.com>
2025-10-24 13:24:15 +0000

    Fix: typo error

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -2,7 +2,7 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
-on tv_show.id = tv_show_genres.show_id
+on tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
 
