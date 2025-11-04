c5ea6d6de5e73ec58a7d8ed09e400367059f4566
shihuaxie <sylvia501628@gmail.com>
2025-11-04 20:50:46 +1100

    Delete negative cases

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 const firstArg = parseInt(process.argv[2]);
 const str = 'C is fun';
 
-if (isNaN(firstArg) || firstArg < 0) {
+if (isNaN(firstArg)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < firstArg; i++) {
