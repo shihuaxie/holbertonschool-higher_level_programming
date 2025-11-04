d11a9336cf0d9392a96b1f26fd4ff1983a088818
shihuaxie <sylvia501628@gmail.com>
2025-11-04 21:15:18 +1100

    add tsk 10 to calculate factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// n! = n × (n - 1) × (n - 2) × ... × 1
+// recursion: factorial(0) = 1
+// factorial(n) = n * factorial(n - 1)
+
+function factorial(n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
