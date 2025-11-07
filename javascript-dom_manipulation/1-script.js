27aef8d710d1f79e3a25d2f466d0646b71722165
shihuaxie <sylvia501628@gmail.com>
2025-11-07 23:22:49 +1100

    Add task 1

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,1 +1,13 @@
-                   
+// select the element where id = red_header
+const redHeader = document.querySelector('#red_header');
+
+// add an eventlistener(click) to redheader
+redHeader.addEventListener('click', changeHeaderColor);
+
+// define the func when the click is triggered
+function changeHeaderColor() {
+  // select header ele
+  const header = document.querySelector('header');
+  // update color
+  header.style.color = '#FF0000';
+}
