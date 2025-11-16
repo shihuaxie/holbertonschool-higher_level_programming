5f64ad5cfd677cb714c561c04030690f2b1336f4
shihuaxie <sylvia501628@gmail.com>
2025-11-16 22:23:57 +1100

    Add tsk1 to create a basic HTML template in Flask

---
+++ python-server_side_rendering/task_01_jinja.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+""" Creating a Basic HTML Template in Flask """
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """Render the home page"""
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    """Render the about page"""
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    """Render the contact page"""
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    # Run on port 5000, debug enabled
+    app.run(debug=True, port=5000)
